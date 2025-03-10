a5e0325ca752cc2defa9b22a14ac41d3f397fdbc
spichay <seanpichay1015@gmail.com>
2025-03-10 13:07:44 -0400

    Feature: add crud operations to userController.js and update routes

--- server/routes/api/api.js
+++ server/routes/api/api.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const { getUser } = require('../../controllers/userController');
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  loginUser
+} = require('../../controllers/userController');
 
 // API routes for user
-router.get('/users', getUser);
+router.post('/create', createUser);
+router.get('/all', auth, getAllUsers);
+router.get('/user/:id', auth, getUserById);
+router.put('/user/:id', auth, updateUser);
+router.delete('/user/:id', auth, deleteUser);
+router.post('/login', loginUser);
 
 // Wild card for when user hits a route that doesn't exist
 router.use((req, res) => {
