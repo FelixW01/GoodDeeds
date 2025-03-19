5301e5b1f55f05f32f973d8c9cdf1e3402aa609f
spichay <seanpichay1015@gmail.com>
2025-03-19 16:45:50 -0400

    Fix: fix test for footer

--- cypress/e2e/footer.cy.js
+++ cypress/e2e/footer.cy.js
@@ -9,45 +9,44 @@ describe('Footer Tests', () => {
         cy.get('footer').should('exist');
 
         // Check if the "About us" link exists and navigates to the correct route
-        cy.contains('About us').should('exist');
+        cy.get('footer a[href="/"]').contains('About us').should('exist');
         cy.get('footer a[href="/"]').contains('About us').click();
         cy.url().should('include', '/');
 
         // Check if the "Contact" link exists and navigates to the correct route
-        cy.contains('Contact').should('exist');
+        cy.get('footer a[href="/"]').contains('Contact').should('exist');
         cy.get('footer a[href="/"]').contains('Contact').click();
         cy.url().should('include', '/');
 
         // Check if the "Events" link exists and navigates to the correct route
-        cy.contains('Events').should('exist');
+        cy.get('footer a[href="/listing"]').contains('Events').should('exist');
         cy.get('footer a[href="/listing"]').contains('Events').click();
         cy.url().should('include', '/listing');
 
         // Check if the "FAQ" link exists and navigates to the correct route
-        cy.contains('FAQ').should('exist');
+        cy.get('footer a[href="/"]').contains('FAQ').should('exist');
         cy.get('footer a[href="/"]').contains('FAQ').click();
         cy.url().should('include', '/');
 
-        // Check if the Twitter icon exists
-        cy.get('footer svg[width="24"][height="24"]')
-            .first()
-            .should('exist')
-            .and('have.attr', 'viewBox', '0 0 24 24');
+        // Check if the social media icons exist and have the correct attributes
+        const socialIcons = [
+            { index: 0, platform: 'Twitter', viewBox: '0 0 24 24' },
+            { index: 1, platform: 'YouTube', viewBox: '0 0 24 24' },
+            { index: 2, platform: 'Facebook', viewBox: '0 0 24 24' },
+        ];
 
-        // Check if the YouTube icon exists
-        cy.get('footer svg[width="24"][height="24"]')
-            .eq(1)
-            .should('exist')
-            .and('have.attr', 'viewBox', '0 0 24 24');
-
-        // Check if the Facebook icon exists
-        cy.get('footer svg[width="24"][height="24"]')
-            .eq(2)
-            .should('exist')
-            .and('have.attr', 'viewBox', '0 0 24 24');
+        socialIcons.forEach((icon) => {
+            cy.get('footer svg[width="24"][height="24"]')
+                .eq(icon.index)
+                .should('exist')
+                .and('have.attr', 'viewBox', icon.viewBox)
+                .and('have.attr', 'class', 'fill-current hover:text-blue-500 hover:scale-110 transition-all duration-300');
+        });
 
         // Check if the copyright text exists and includes the current year
         const currentYear = new Date().getFullYear();
-        cy.contains(`Copyright © ${currentYear} - All right reserved by GoodDeeds Ltd`).should('exist');
+        cy.get('footer aside p')
+            .should('exist')
+            .and('have.text', `Copyright © ${currentYear} - All right reserved by GoodDeeds Ltd`);
     });
 });
